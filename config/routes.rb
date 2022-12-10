2ee867dcf559d9f307faec50ea4c70d594a1e4c2
sailbirds <sailbirds@luminesence.jp>
2022-12-10 21:12:57 +0900

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'user/create'
-  get 'user/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      # /api/v1
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources :items
+      resources :tags
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  post '/users', to: 'users#create'
-  get '/users/:id', to: 'users#show'
 end
